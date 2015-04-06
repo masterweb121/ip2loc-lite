4b7fe8e23da8b23534870278ca2de84afafe3145
Nem C <nemc@codeanvil.co>
2015-04-06 13:45:31 +0200

    Trim IP before doing long2ip because of possible failures

--- src/IP2LocLite/Facades/IP2LocLite.php
+++ src/IP2LocLite/Facades/IP2LocLite.php
@@ -8,9 +8,15 @@ class IP2LocLite extends Facade
 {
     public static function get($ip)
     {
+        //trim IP just in case
+        $ip = trim($ip);
         $longIp = ip2long($ip);
+        if ($longIp !== false) {
+            return static::$app['nemc_ip2loc-lite_helper']->getForLongIp($longIp);
+        } else {
+            return [];
+        }
 
-        return static::$app['nemc_ip2loc-lite_helper']->getForLongIp($longIp);
     }
     protected static function getFacadeAccessor()
     {
